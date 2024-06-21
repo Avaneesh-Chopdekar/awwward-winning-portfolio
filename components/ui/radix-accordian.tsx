d522337fab7afc4a553949728d37b4624aa4710d
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-06-21 20:09:17 +0530

    added mild background color to radix accordian.item

--- components/ui/radix-accordian.tsx
+++ components/ui/radix-accordian.tsx
@@ -23,7 +23,7 @@ export default function RadixAccordion(props: RadixAccordianProps) {
         <Accordion.Item
           key={item.title}
           value={item.title}
-          className="mb-2 rounded-md border px-8 py-4"
+          className="mb-2 rounded-md border border-black/[0.1] bg-gray-50 px-8 py-4 transition-colors ease-in-out hover:border-black/[0.5] dark:border-white/[0.2] dark:bg-black"
         >
           <Accordion.Header>
             <Accordion.Trigger className="group flex w-full flex-1 items-center justify-between gap-6 text-start">
@@ -34,7 +34,7 @@ export default function RadixAccordion(props: RadixAccordianProps) {
               />
             </Accordion.Trigger>
           </Accordion.Header>
-          <Accordion.Content className="data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp mt-2 overflow-hidden text-neutral-500">
+          <Accordion.Content className="mt-2 overflow-hidden text-neutral-500 data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown dark:text-neutral-300">
             {item.description}
           </Accordion.Content>
         </Accordion.Item>

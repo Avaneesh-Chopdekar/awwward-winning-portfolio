6fc9d001f3fd23674fa3c5ecaf80912efaab9e81
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-07-01 17:31:23 +0530

    create sitemap.ts and robots.ts file for seo

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from "next";
+import { posts } from "@velite/content";
+import { sortPostsDescendingByDate } from "@/lib/utils";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const sortedPosts = sortPostsDescendingByDate(
+    posts.filter((post) => post.published),
+  );
+
+  const postEntries: MetadataRoute.Sitemap = sortedPosts.map(({ slug }) => ({
+    url: `${process.env.NEXT_PUBLIC_BASE_URL}/${slug}`,
+  }));
+
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
+    },
+    ...postEntries,
+  ];
+}

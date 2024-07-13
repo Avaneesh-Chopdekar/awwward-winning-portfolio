1794d2ab2a358e7b416f0966ad76f375c2d33677
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-07-13 20:36:48 +0530

    fixed nav links back to normal

--- components/ui/navbar/navLinks.ts
+++ components/ui/navbar/navLinks.ts
@@ -5,15 +5,15 @@ export type NavLinkType = {
 
 export const navLinks: NavLinkType[] = [
   {
-    title: "One",
+    title: "Home",
     href: "/",
   },
   {
-    title: "Two",
+    title: "Blog",
     href: "/blog",
   },
   {
-    title: "Three",
+    title: "Chat",
     href: "/chat",
   },
   // {
@@ -21,7 +21,7 @@ export const navLinks: NavLinkType[] = [
   //   href: "/games",
   // },
   {
-    title: "Four",
+    title: "Resume",
     href: "https://www.google.com/",
   },
 ];

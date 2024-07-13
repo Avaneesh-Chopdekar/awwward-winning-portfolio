2e1fca40b3f98b05bc9c7bca8ee10eba52cf1129
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-07-13 17:53:51 +0530

    cleaned up menu.tsx by creating seperate files for navLinks and variants

--- components/ui/navbar/menu.tsx
+++ components/ui/navbar/menu.tsx
@@ -1,84 +1,18 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import { AnimatePresence, motion, Variants } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+
+import { navLinks } from "./navLinks";
+import { containerVariants, linkVariants, menuVariants } from "./variants";
 
 // TODO: Add hover.dev reveal links later
-
-const navLinks = [
-  {
-    title: "Home",
-    href: "/",
-  },
-  {
-    title: "Blog",
-    href: "/blog",
-  },
-  {
-    title: "Chat",
-    href: "/chat",
-  },
-];
 
 export default function Menu() {
   const [open, setOpen] = useState(false);
   function toggleMenu() {
     setOpen((prevState) => !prevState);
   }
-
-  const menuVariants: Variants = {
-    initial: {
-      scaleY: 0,
-    },
-    animate: {
-      scaleY: 1,
-      transition: {
-        duration: 0.5,
-        ease: "easeInOut",
-      },
-    },
-    exit: {
-      scaleY: 0,
-      transition: {
-        delay: 0.5,
-        duration: 0.5,
-        ease: [0.22, 1, 0.36, 1],
-      },
-    },
-  };
-
-  const containerVariants: Variants = {
-    initial: {
-      transition: {
-        staggerChildren: 0.09,
-        staggerDirection: -1,
-      },
-    },
-    open: {
-      transition: {
-        delayChildren: 0.3,
-        staggerChildren: 0.09,
-        staggerDirection: 1,
-      },
-    },
-  };
-
-  const linkVariants: Variants = {
-    initial: {
-      y: "30vh",
-      transition: {
-        duration: 0.5,
-        ease: [0.37, 0, 0.63, 1],
-      },
-    },
-    open: {
-      y: 0,
-      transition: {
-        duration: 0.7,
-        ease: [0, 0.55, 0.45, 1],
-      },
-    },
-  };
 
   return (
     <div>
---
+++ components/ui/navbar/navLinks.ts
@@ -0,0 +1,27 @@
+export type NavLinkType = {
+  title: string;
+  href: string;
+};
+
+export const navLinks: NavLinkType[] = [
+  {
+    title: "Home",
+    href: "/",
+  },
+  {
+    title: "Blog",
+    href: "/blog",
+  },
+  {
+    title: "Chat",
+    href: "/chat",
+  },
+  // {
+  //   title: "Games",
+  //   href: "/games",
+  // },
+  {
+    title: "Resume",
+    href: "https://www.google.com/",
+  },
+];
